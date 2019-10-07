d33cdcb40bb33a40f08708f7d0ed5cda6816efce
chandano <chandano@mkcl.org>
2019-10-07 10:28:26 +0530

    tushar - 10.28

---
+++ src/views/HomePage.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <div
+      id="carouselExampleControls"
+      class="carousel slide"
+      data-ride="carousel"
+      data-interval="5000"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            class="d-block w-100"
+            src="slide1.jpg"
+            alt="First slide"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100"
+            src="slide2.jpg"
+            alt="Second slide"
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            class="d-block w-100"
+            src="slide3.jpg"
+            height="700px"
+            alt="Third slide"
+          />
+        </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#carouselExampleControls"
+        role="button"
+        data-slide="prev"
+      >
+        <span
+          class="carousel-control-prev-icon"
+          aria-hidden="true"
+        ></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#carouselExampleControls"
+        role="button"
+        data-slide="next"
+      >
+        <span
+          class="carousel-control-next-icon"
+          aria-hidden="true"
+        ></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    <div class="container">
+      <div class="detailss row">
+        <div
+          class="card"
+          style="width: 20rem;"
+        >
+          <img
+            src="card1.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Shopping Made Easy</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a
+              href="/register"
+              class="btn btn-primary"
+            >Register</a>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          style="width: 20rem;"
+        >
+          <img
+            src="card2.jpg"
+            class="card-img-top"
+            height="235px;"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Add To Cart</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div
+          class="card"
+          style="width: 20rem;"
+        >
+          <img
+            src="card3.jpg"
+            class="card-img-top"
+            height="235px;"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">Market At Home</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a
+              href="/login"
+              class="btn btn-primary"
+            >Login</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mainfoot">
+      <div class="foot container">
+        <center><a href="#">@Copyrights HaryanaState.gov.in</a></center>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.carousel-inner {
+  height: 700px;
+}
+.carousel {
+  margin-top: 1px;
+}
+.detailss {
+  justify-content: space-around;
+  margin-top: 20px;
+}
+.foot {
+  background-color: #282828;
+  color: white;
+  padding: 20px;
+  margin-top: 50px;
+}
+.foot a {
+  color: white;
+  text-decoration: none;
+}
+.mainfoot {
+  background-color: #282828;
+  color: white;
+}
+</style>
--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,82 +1,65 @@
 <template>
-  <!--Navbar -->
-  <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color bg-info">
-    <a class="navbar-brand" href="#">Haryana E-Mart</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent-333"
-      aria-controls="navbarSupportedContent-333"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-      <ul class="navbar-nav mr-auto">
-        <!-- <li class="nav-item active">
-        <a class="nav-link" href="#">Home
-          <span class="sr-only">(current)</span>
-        </a>
-        </li>-->
-        <!-- <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-        </li>-->
-        <!-- <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-        </li>-->
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            id="navbarDropdownMenuLink-333"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-            >Order-Info</a
-          >
-          <div
-            class="dropdown-menu dropdown-default"
-            aria-labelledby="navbarDropdownMenuLink-333"
-          >
-            <a class="dropdown-item" href="#">Order Details</a>
-          </div>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto nav-flex-icons">
-        <li class="nav-item">
-          <a class="nav-link waves-effect waves-light">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link waves-effect waves-light">
-            <i class="fab fa-google-plus-g"></i>
-          </a>
-        </li>
-        <b-nav-item-dropdown right>
-          <ul class="navbar-nav ml-auto nav-flex-icons">
-            <li class="nav-item">
-              <a class="nav-link waves-effect waves-light">
-                <i class="fab fa-twitter"></i>
-              </a>
-            </li>
-          </ul>
+  <div>
+    <div class="pos-f-t">
+      <div
+        class="collapse"
+        id="navbarToggleExternalContent"
+      >
+        <div class="bg-dark p-4">
+          <h4 class="text-white">About Us</h4>
+          <span class="text-white">Haryana E-market is a palce where everyone can get the online orignal
+            product produced in Haryana state.
+          </span>
+        </div>
+      </div>
+      <nav class="navbar navbar-dark bg-dark">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggleExternalContent"
+          aria-controls="navbarToggleExternalContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <h4 style="color:white">Haryana E-Mart</h4>
+        </button>
+        <div class="log1">
+          <h6>
+            <a href="/"><span class="glyphicon glyphicon-user"></span> Home</a>
+            <a href="/register"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
 
-          <template v-slot:button-content>
-            <BaseIcon name="user"></BaseIcon>
-            <em></em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </ul>
+            <a href="/login"><span class="glyphicon glyphicon-log-in"></span> Login</a>
+            <!-- <a href="/"><span class="glyphicon glyphicon-user"></span> Home</a>
+            <a href="#"><span class="glyphicon glyphicon-user"></span> All Products</a>
+            <a href="#"><span class="glyphicon glyphicon-user"></span> Order History</a>
+            <a href="#"><span class="glyphicon glyphicon-user"></span> Cart</a>
+            <a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a> -->
+
+          </h6>
+        </div>
+      </nav>
     </div>
-  </nav>
-  <!--/.Navbar -->
+  </div>
 </template>
+
 <script>
 export default {
   name: 'navBar'
 }
 </script>
+<style scoped>
+.log1 a {
+  color: white;
+  padding: 20px;
+  text-decoration: none;
+}
+
+.log1 a:hover {
+  background-color: grey;
+}
+.log {
+  padding: 10px;
+  display: block;
+}
+</style>
